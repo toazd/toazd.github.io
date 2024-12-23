4e8a6d46da2e1497578fad05f2da33374bb57c73
toazd <wmcdannell@hotmail.com>
2024-12-23 15:26:04 -0500

    update

--- script.js
+++
@@ -1,9 +0,0 @@
-let upBtn = document.getElementById('scrollbuttonid');
-
-window.addEventListener('scroll', function () {
-    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
-        upBtn.style.display = 'block';
-    } else {
-        upBtn.style.display = 'none';
-    }
-});
