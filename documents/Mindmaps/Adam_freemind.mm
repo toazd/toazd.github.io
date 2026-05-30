00aa0141a0df0854cab72542c47e863704027949
toazd <wmcdannell@gmail.com>
2026-05-30 14:28:34 -0400

    Add Bible study notes backup and automated indexing workflow

---
+++ documents/Mindmaps/Adam_freemind.mm
@@ -0,0 +1,506 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#111111" CREATED="1683996386528" ID="ID_873626502" MODIFIED="1684010770699" STYLE="bubble" TEXT="Adam">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:5
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+<attribute NAME="Age" VALUE="930"/>
+<node CREATED="1683996779628" ID="ID_1873288547" MODIFIED="1683998018362" POSITION="right" TEXT="Eve" VGAP="4">
+<icon BUILTIN="female2"/>
+<node CREATED="1683996786715" HGAP="23" ID="ID_1032367370" MODIFIED="1683999919515" TEXT="Cain" VSHIFT="76">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:1
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683997789754" HGAP="13" ID="ID_1814140612" MODIFIED="1684000820844" TEXT="Enoch" VSHIFT="24">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:17
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683997846298" HGAP="14" ID="ID_199417177" MODIFIED="1684000864148" TEXT="Irad" VSHIFT="26">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:18
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683997875819" HGAP="13" ID="ID_776688022" MODIFIED="1684000872754" TEXT="Mehujael" VSHIFT="26">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:18
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683997889034" HGAP="13" ID="ID_1141705083" MODIFIED="1684000877243" TEXT="Methusael" VSHIFT="25">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:18
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683997909074" HGAP="13" ID="ID_1046629320" MODIFIED="1684000879819" TEXT="Lamech" VSHIFT="23">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:18
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+<node CREATED="1683998081803" HGAP="18" ID="ID_1791993883" MODIFIED="1684000900067" TEXT="Adah" VSHIFT="2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:19
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="female2"/>
+<node CREATED="1683998126186" HGAP="21" ID="ID_1749173504" MODIFIED="1684000920603" TEXT="Jabal" VSHIFT="-2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:20
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1683998216547" ID="ID_1754250790" MODIFIED="1684000935891" TEXT="Jubal" VSHIFT="-5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:21
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node CREATED="1683998097754" HGAP="18" ID="ID_932293756" MODIFIED="1684000906461" TEXT="Zillah">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:19
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="female2"/>
+<node CREATED="1683998253834" ID="ID_1561701432" MODIFIED="1684000944491" TEXT="Tubal-cain (assumed male)">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:22
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1683998295754" ID="ID_1156054335" MODIFIED="1684000954523" TEXT="Naamah">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:22
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="female2"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1683996797940" HGAP="31" ID="ID_946363812" MODIFIED="1683999941965" TEXT="Abel" VSHIFT="-1">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:2
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1683998418658" HGAP="29" ID="ID_322301640" MODIFIED="1684011470015" TEXT="Seth" VSHIFT="-19">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:25, 5:3, 5:8
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="34" VALUE_WIDTH="34"/>
+<attribute NAME="Age" VALUE="912"/>
+<attribute NAME="Adam" VALUE="130"/>
+<node CREATED="1683998481154" HGAP="13" ID="ID_1358931886" MODIFIED="1684010891166" TEXT="Enos" VSHIFT="35">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 4:26, 5:6, 5:11
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+<attribute NAME="Age" VALUE="905"/>
+<attribute NAME="Seth" VALUE="105"/>
+<node CREATED="1683998864866" HGAP="17" ID="ID_1372995244" MODIFIED="1684010947201" TEXT="Cainan" VSHIFT="39">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:9, 5:14
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+<attribute NAME="Age" VALUE="910"/>
+<attribute NAME="Enos" VALUE="90"/>
+<node CREATED="1683998961594" HGAP="17" ID="ID_436109586" MODIFIED="1684010978649" TEXT="Mahalaleel" VSHIFT="35">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:12, 5:17
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="38" VALUE_WIDTH="38"/>
+<attribute NAME="Age" VALUE="895"/>
+<attribute NAME="Cainan" VALUE="70"/>
+<node CREATED="1683999194898" HGAP="18" ID="ID_1176333888" MODIFIED="1684011083161" TEXT="Jared" VSHIFT="38">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:15, 5:20
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="57" VALUE_WIDTH="57"/>
+<attribute NAME="Age" VALUE="962"/>
+<attribute NAME="Mahalaleel" VALUE="65"/>
+<node CREATED="1684000670297" HGAP="18" ID="ID_1433657935" MODIFIED="1684011677785" TEXT="Enoch" VSHIFT="34">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:18, 5:23, Jude 1:14
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="33" VALUE_WIDTH="33"/>
+<attribute NAME="Age" VALUE="365"/>
+<attribute NAME="Jared" VALUE="162"/>
+<node CREATED="1684001483552" HGAP="15" ID="ID_34269785" MODIFIED="1684011752066" TEXT="Methuselah" VSHIFT="31">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:21, 5:27
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="37" VALUE_WIDTH="37"/>
+<attribute NAME="Age" VALUE="969"/>
+<attribute NAME="Enoch" VALUE="65"/>
+<node CREATED="1684001666616" HGAP="25" ID="ID_463209996" MODIFIED="1684011804992" TEXT="Lamech" VSHIFT="33">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:25, 5:31
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="65" VALUE_WIDTH="65"/>
+<attribute NAME="Age" VALUE="777"/>
+<attribute NAME="Methuselah" VALUE="187"/>
+<node CREATED="1684002317135" ID="ID_360401973" MODIFIED="1684006129858" TEXT="Noah" VSHIFT="33">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:28-29, Genesis 7:11
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+<attribute_layout NAME_WIDTH="45" VALUE_WIDTH="45"/>
+<attribute NAME="Flood" VALUE="600"/>
+<attribute NAME="Age" VALUE="950"/>
+<attribute NAME="Lamech" VALUE="182"/>
+<node CREATED="1684005113798" HGAP="19" ID="ID_1217865347" MODIFIED="1684005953097" TEXT="Ham" VSHIFT="21">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:32, 9:22-24
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1684005122294" HGAP="21" ID="ID_1939039243" MODIFIED="1684005955576" TEXT="Shem">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:32, 10:21
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+</node>
+<node CREATED="1684005128398" HGAP="22" ID="ID_1431637433" MODIFIED="1684005959056" TEXT="Japeth" VSHIFT="2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:32, 10:21
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="male1"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="25" ID="ID_714449211" MODIFIED="1684002396097" TEXT="Sons &amp; Daughters" VSHIFT="4">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:30
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="25" ID="ID_1987283850" MODIFIED="1684001860111" TEXT="Sons &amp; Daughters" VSHIFT="4">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:26
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="19" ID="ID_1358133173" MODIFIED="1684001848522" TEXT="Sons &amp; Daughters" VSHIFT="4">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:22
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="17" ID="ID_1792105658" MODIFIED="1684005890644" TEXT="Sons &amp; Daughters" VSHIFT="-5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:19
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="17" ID="ID_1442741772" MODIFIED="1684000442107" TEXT="Sons &amp; Daughters" VSHIFT="-5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:16
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="17" ID="ID_1467981755" MODIFIED="1684000371164" TEXT="Sons &amp; Daughters" VSHIFT="-5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:13
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000270392" HGAP="17" ID="ID_1630634642" MODIFIED="1684000299464" TEXT="Sons &amp; Daughters" VSHIFT="-5">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:10
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+<node CREATED="1684000192857" HGAP="27" ID="ID_838573194" MODIFIED="1684000304096" TEXT="Sons &amp; Daughters">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:7
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+</node>
+<node CREATED="1683999401786" HGAP="-142" ID="ID_194126577" MODIFIED="1684000206006" POSITION="left" TEXT="Sons &amp; Daughters" VSHIFT="57">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Genesis 5:4
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="fema"/>
+</node>
+</node>
+</map>
---
+++ documents/Mindmaps/Adam_freeplane.mm
@@ -0,0 +1,685 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1721045218543" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm" show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<node POSITION="bottom_or_right" ID="ID_1776223291" CREATED="1721045171722" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: #111111;">Adam</span>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<edge COLOR="#ff0000"/>
+<node ID="ID_670447021" CREATED="1721045171722" MODIFIED="1721045227583"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_596113028" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Eve
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1191994757" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Cain
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1477443761" CREATED="1721045171722" MODIFIED="1721045232929"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Enoch
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1294455108" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Irad
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_572733661" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mehujael
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_390629841" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Methusael
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_387792226" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Lamech
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_493718005" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Adah
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1363524170" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Jabal
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_59884520" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Jubal
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1335990180" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Zillah
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1930106524" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Tubal-cain (assumed male)
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1132732239" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Naamah
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node ID="ID_1453162575" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Abel
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1750765845" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Seth
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1697513576" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Enos
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_355908217" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Cainan
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_797977801" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Mahalaleel
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_784947200" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Jared
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_263821491" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Enoch
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_490421228" CREATED="1721045171722" MODIFIED="1721045171722"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Methuselah
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1786754892" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Lamech
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_1348736072" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Noah
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+<node ID="ID_110370220" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Ham
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_1180871067" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Japeth
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_469845469" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Shem
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_434720081" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1424003345" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1635777126" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1995631263" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_137942462" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1896013670" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_1538805875" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node ID="ID_747157903" CREATED="1721045171738" MODIFIED="1721045171738"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Sons &amp; Daughters
+      </li>
+    </ul>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
