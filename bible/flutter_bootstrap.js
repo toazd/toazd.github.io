71e03c2d7c786c2c2b8a898babdacaa07f9d6a49
toazd <wmcdannell@gmail.com>
2026-06-11 16:14:00 -0400

    Update to latest version of Selah (0.7.6)

--- bible/flutter_bootstrap.js
+++ bible/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3933235676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "16134139" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
