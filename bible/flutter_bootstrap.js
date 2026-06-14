a6b41313fdb139b10d9bd03734999de5c40aa18b
toazd <wmcdannell@gmail.com>
2026-06-14 09:07:33 -0400

    update

--- bible/flutter_bootstrap.js
+++ bible/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2312335862" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2528343449" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
